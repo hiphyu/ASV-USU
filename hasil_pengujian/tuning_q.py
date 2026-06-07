b6fee162c27e428d6ee4a36201a01ffe9c9d8393
Stephany <stephany@students.usu.ac.id>
2026-06-07 14:06:44 +0700

    update

--- hasil_pengujian/tuning_q.py
+++ hasil_pengujian/tuning_q.py
@@ -74,8 +74,6 @@ def run_pure_ekf(Q_diag,
         F[0,4] = (-vx*sy - vy*cy)*dt
         F[1,2] =  sy*dt;  F[1,3] =  cy*dt
         F[1,4] = ( vx*cy - vy*sy)*dt
-        F[2,2] = 0.98
-        F[3,3] = 0.98
         F[4,5] = dt
         P[:] = F @ P @ F.T + Q
 
